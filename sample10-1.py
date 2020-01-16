43d746d91c128a066f4a2bc10dfc97b6306cd568
槙翔大 <makishoudai@makishoudainoiMac.local>
2020-01-16 18:24:40 +0900

    指定したインデックスの要素を取得する

---
+++ sample10-1.py
@@ -0,0 +1,13 @@
+str1 = "Hello"
+print(str1[0])
+print(str1[1])
+print(str1[2])
+print(str1[3])
+print(str1[4])
+
+str2 = "こんにちは"
+print(str2[-5])
+print(str2[-4])
+print(str2[-3])
+print(str2[-2])
+print(str2[-1])
