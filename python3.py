d01548728cb9f297944ead8cd53ef494cb770c48
槙翔大 <makishoudai@makishoudainoiMac.local>
2020-01-18 19:56:57 +0900

    繰り返し実行for構文

---
+++ python3.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+for b in [1,2,3,4,5]:
+    print(b)
+    print("こんにちは")
+
+    for a in range(1,100 + 1):
+        print(a)
+        print("こんにちは")
+for c in "Hello":
+    print(c)
+    
