b25c6e8a3f8e89abe9a164cf55bc68e0a568b72b
maki216syou <maki216syou@gmail.com>
2020-02-06 17:27:20 +0900

    p179

--- python_book/example06-06-01.py
+++ python_book/example06-06-01.py
@@ -20,30 +20,33 @@ def ButtonClick():
                 tmsg.showerror("エラー","数字ではありません")
                 kazuok = False
                 break
-        if isok :
-            # ４桁の数字であったとき
-            # ヒットを判定
-            hit = 0
-            for i in range(4):
-                if a[i] == int(b[i]):
-                    hit = hit + 1
+            if kazuok:
+                isok = True
 
-            #　ブローを判定
-            blow = 0
-            for j in range(4):
-                for J in range(4):
-                    if(int(b[j]) == a[i]) and (a[i] != int(b[i])) and (a[j] != int(b[j])):
-                        blow = blow + 1
-                        break
+    if isok :
+        # ４桁の数字であったとき
+        # ヒットを判定
+        hit = 0
+        for i in range(4):
+            if a[i] == int(b[i]):
+                hit = hit + 1
 
-            # ヒットが４なら当たりで終了
-            if hit == 4:
-                tmsg.showinfo("当たり","おめでとうございます。当たりです")
-                # 終了
-                root.destroy()
-            else:
-                # ヒット数とブロー数を表示
-                rirekibox.insert(tk.END,b + " /H:" + str(hit) + " B:" + str(blow) + "\n")
+        #　ブローを判定
+        blow = 0
+        for j in range(4):
+            for J in range(4):
+                if(int(b[j]) == a[i]) and (a[i] != int(b[i])) and (a[j] != int(b[j])):
+                    blow = blow + 1
+                    break
+
+        # ヒットが４なら当たりで終了
+        if hit == 4:
+            tmsg.showinfo("当たり","おめでとうございます。当たりです")
+            # 終了
+            root.destroy()
+        else:
+            # ヒット数とブロー数を表示
+            rirekibox.insert(tk.END,b + " /H:" + str(hit) + " B:" + str(blow) + "\n")
 
 # メインのプログラム
 # 最初にランダムな4つの数字を作成しておく
