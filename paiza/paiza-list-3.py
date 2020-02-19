54a6c5454c0a6b05f662f60e4e2528d8d9c21966
maki216syou <maki216syou@gmail.com>
2020-02-19 22:26:45 +0900

    リストの要素を取り出してみよう

---
+++ paiza/paiza-list-3.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+# リストの要素を取り出す
+
+team = ["勇者", "魔法使い", "戦士"]
+print(team)
+print(team[1])
+num = 0
+print(team[num + 1])
+#print(team[10])
+print(len(team))
+
+# 演習課題１
+
+team2 = ["勇者", "戦士", "侍", "忍者", "魔法使い"]
+print(team[0])
+
+# 演習課題2
+
+team3 = ["勇者", "戦士", "侍", "忍者", "魔法使い"]
+print(team3[2])
+
+# 演習課題3
+
+weapon = ["木の棒", "鉄の棒", "鉄の剣", "銅の剣", "石斧", "エクスカリバー"]
+print(len(weapon))
