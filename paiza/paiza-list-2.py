8565a501d066516b8f48b216568fef1c8f38d514
maki216syou <maki216syou@gmail.com>
2020-02-19 19:57:37 +0900

    リストの作成　paiza

---
+++ paiza/paiza-list-2.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+# リストを作成する
+
+player_1 = "戦士"
+player_2 = "魔法使い"
+
+print(player_1)
+print(player_2)
+
+team = ["勇者", "魔法使い", 100, player_1]
+print(team)
+
+# 演習課題　1
+list = ["戦士", "侍", "僧侶", "魔法使い"]
+print(list)
+
+
+# 演習課題　２
+item = ["ロングソード", "ブレードソード", "エクスカリバー"]
+print(item)
+
+#　演習課題　3
+player_4 = "勇者"
+player_5 = "魔法使い"
+player_6 = "戦士"
+
+list2 = [player_4, player_5, player_6]
